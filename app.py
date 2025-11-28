3c5132525543076a1f6bb97247088c44b1a6d47c
Manuel_Sambrano_Mendoza <25460335@colima.tecmn.mx>
2025-11-28 13:49:48 -0600

    28 de noviembre

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title("Hola mundo!! ")
+st.write("Esta es mi primera aplicación web creada con streamlit")
+
+st.subheader("Qué hace esta aplicación?")
+st.write(
+    """"
+    -Muestra un título en la parte superior
+    -Muestra un texto de bienvenida
+    -Corre como una app web en el navegador
+    """
+)
+
+st.success("!Felicidades¡ ya tienes tu primera app web con streamlit.")
